4fe5d4b4a193e6db5fa4c9b2627f1198ce76b4c3
meynguy <meynguy@users.noreply.github.com>
2023-12-16 21:32:48 +0100

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,5 +11,5 @@ import { FormsModule } from '@angular/forms';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'learn-angular-app';
+  title = 'learn-angular-appy';
 }
